f58e7b03a0da5ee35e9730f55d7d0fb250bbede6
teomores <mattemoresc@gmail.com>
2020-03-03 19:38:01 +0100

    scraper aggiornato

--- scrapers/betexplorer_scraper.py
+++ scrapers/betexplorer_scraper.py
@@ -5,6 +5,20 @@ from datetime import datetime
 from selenium.common.exceptions import NoSuchElementException
 import pickle
 import os
+from csv import DictWriter
+
+"""
+Questo script scrapa betexplorer per gli anni passati. 
+Dal 2019/2020 hanno iniziato a mettere anche le quote giorno per giorno, per fare scraping
+anche di quelle:
+    try:
+        odds.click()
+        time.sleep(2)
+        element = driver.find_element_by_xpath('//*[@id="js-popup"]/div/div/div/span')
+        driver.execute_script("arguments[0].click();", element) # perché c'è del js
+    except NoSuchElementException:
+        pass
+"""
 
 BASE_URL = 'https://www.betexplorer.com/soccer/'
 
@@ -26,31 +40,44 @@ def _scrape_league_matches(league_url: str) -> list:
     for row in table.find_elements_by_css_selector('tr'):
         if row.text[0].isdigit() == False:
             url = row.find_elements_by_css_selector('a')[0].get_attribute('href')
-            match_urls_list.append(url)
+            match_urls_list.append((url,match_round))
+        else:
+            match_round = row.text.split('.')[0]
     print(f'Total matches: {len(match_urls_list)}')
-
-    with open('betexplorer_urls', 'wb') as fp:
-        pickle.dump(match_urls_list, fp)
     return match_urls_list
 
 def scrape(country: str, league: str, start_year: int) -> None:
     # create pandas dataframe
-    columns = ['current_timestamp','match_date','match_day', # per ora attacco il timestamp, poi pulirò
-                'match_hour','team_1','team_2','bookmaker','1','X','2']
+    columns = ['match_date', 'match_hour', 'match_round', 'home_team','away_team','home_goals','away_goals','bookmaker','1','X','2','opening_1','opening_X','opening_2']
     df_results = pd.DataFrame(columns=columns)
+    with open(f'{league}_{start_year}.csv', 'a+', newline='') as f:
+                    dict_writer = DictWriter(f, fieldnames=df_results.columns)
+                    dict_writer.writerow(
+                        {
+                        'match_date' : 'match_date',
+                        'match_hour' : 'match_hour',
+                        'match_round' : 'match_round',
+                        'home_team' : 'home_team',
+                        'away_team' : 'away_team',
+                        'home_goals' : 'home_goals',
+                        'away_goals' : 'away_goals',
+                        'bookmaker' : 'bookmaker',
+                        '1' : '1',
+                        'X' : 'X',
+                        '2' : '2',
+                        'opening_1' : 'opening_1',
+                        'opening_X' : 'opening_X',
+                        'opening_2' : 'opening_2'
+                        }
+                    )
 
-    try:
-        with open ('betexplorer_urls', 'rb') as fp:
-            match_urls_list = pickle.load(fp)
-    except IOError:
-        # se non c'è il file con tutte le urls me lo ricreo
-        league_url = BASE_URL + f'{country}/{league}-{start_year}-{start_year+1}/' # url to get matches from
-        match_urls_list = _scrape_league_matches(league_url)
+    league_url = BASE_URL + f'{country}/{league}-{start_year}-{start_year+1}/' # url to get matches from
+    match_urls_list = _scrape_league_matches(league_url)
 
     # get ChromeDriver
     driver = webdriver.Chrome('chromedriver.exe')
     count = 0
-    for url in match_urls_list[:1]:
+    for url, match_round in match_urls_list:
         print('==============================================')
         driver.get(url)
         match_teams = driver.find_element_by_xpath('/html/body/div[4]/div[4]/div/div/div[1]/section/ul[1]/li[5]/span').text.split('-')
@@ -60,22 +87,57 @@ def scrape(country: str, league: str, start_year: int) -> None:
         match_result =  driver.find_element_by_xpath('//*[@id="js-score"]').text.split(':')
         home_goals, away_goals = match_result[0].strip(), match_result[1].strip() 
         count += 1
-        print(f'{count}: {home_team} - {away_team} - {date} - {hour} - {home_goals}:{away_goals}')
+        print(f'{count}/{len(match_urls_list)}: {match_round} - {home_team} - {away_team} - {date} - {hour} - {home_goals}:{away_goals}')
 
         driver.implicitly_wait(0.5)
         table = driver.find_elements_by_id('sortable-1')[0]
+        bookmaker = hw_odd = hw_opening_odd = draw_odd = draw_opening_odd = aw_odd = aw_opening_odd =''
         for row in table.find_elements_by_css_selector('tr')[:-1]: # tolgo l'ultima perché fa average odds
             for i,odds in enumerate(row.find_elements_by_css_selector('td')):
                 if i == 0:
-                    print(odds.text)
+                    bookmaker = odds.text.lower()
+                    #print(f"BOOKMAKER: {bookmaker}")
                 elif i == 1 or i == 2:
                     pass
-                else:
-                    print(f"ODD: {odds.get_attribute('data-odd')}")
-                    print(f"OPENING ODD: {odds.get_attribute('data-opening-odd')}")
+                elif i == 3:
+                    hw_odd = odds.get_attribute('data-odd')
+                    #print(f"1 ODD: {hw_odd}")
+                    hw_opening_odd = odds.get_attribute('data-opening-odd')
+                    #print(f"1 OPENING ODD: {hw_opening_odd}")
+                elif i == 4:
+                    draw_odd = odds.get_attribute('data-odd')
+                    #print(f"X ODD: {draw_odd}")
+                    draw_opening_odd = odds.get_attribute('data-opening-odd')
+                    #print(f"X OPENING ODD: {draw_opening_odd}")
+                elif i ==5:
+                    aw_odd = odds.get_attribute('data-odd')
+                    #print(f"2 ODD: {aw_odd}")
+                    aw_opening_odd = odds.get_attribute('data-opening-odd')
+                    #print(f"2 OPENING ODD: {aw_opening_odd}")
+            if bookmaker != '':
+                with open(f'{league}_{start_year}.csv', 'a+', newline='') as f:
+                    dict_writer = DictWriter(f, fieldnames=df_results.columns)
+                    dict_writer.writerow(
+                        {
+                        'match_date' : date,
+                        'match_hour' : hour,
+                        'match_round' : match_round,
+                        'home_team' : home_team,
+                        'away_team' : away_team,
+                        'home_goals' : home_goals,
+                        'away_goals' : away_goals,
+                        'bookmaker' : bookmaker,
+                        '1' : hw_odd,
+                        'X' : draw_odd,
+                        '2' : aw_odd,
+                        'opening_1' : hw_opening_odd,
+                        'opening_X' : draw_opening_odd,
+                        'opening_2' : aw_opening_odd
+                        }
+                    )
 
 
 
 
 if __name__=='__main__':
-    scrape('italy','serie-a',2018)
+    scrape('italy','serie-a',2017)
